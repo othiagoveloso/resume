0baff44fb4475c573a60f556c774ec6b6d012eaf
Thiago Veloso <thiago.veloso@MacBook-Air-de-Thiago.local>
2020-05-26 12:25:14 -0300

    Updates

---
+++ precache-manifest.d01e560f5fc66be6015d111cf36b846c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feae672bcd314216d0e638e50c1597a0",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "1ab78b74ce2a45c65a2c",
+    "url": "/resume/static/css/main.639fa828.chunk.css"
+  },
+  {
+    "revision": "8ce949436e8949522e48",
+    "url": "/resume/static/js/2.cd6da13d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.cd6da13d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab78b74ce2a45c65a2c",
+    "url": "/resume/static/js/main.049d26a7.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  }
+]);
